491ac8a36c978f2f853a118f070a02bcea1e3792
bob rohan <bob rohan@LINKMGTLDMS8.wales.linkfinancial.eu>
2017-08-14 10:00:40 +0100

    test commit

--- angular-ui/src/main/java/rohan/oauth2/angularui/AngularUIApplication.java
+++ angular-ui/src/main/java/rohan/oauth2/angularui/AngularUIApplication.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+// test
 @SpringBootApplication
 @EnableOAuth2Sso
 public class AngularUIApplication extends WebSecurityConfigurerAdapter {
