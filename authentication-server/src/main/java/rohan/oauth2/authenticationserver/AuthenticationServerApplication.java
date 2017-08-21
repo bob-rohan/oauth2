3a7e48f341501e8c8d78ac3b4515a211f06c16cc
bob rohan <bobbyrohan@gmail.com>
2017-08-21 11:27:09 +0100

    #5 mvc-ui to use authorization_code grant flow

--- authentication-server/src/main/java/rohan/oauth2/authenticationserver/AuthenticationServerApplication.java
+++ authentication-server/src/main/java/rohan/oauth2/authenticationserver/AuthenticationServerApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.web.cors.CorsConfiguration;
@@ -22,72 +27,110 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
-@EnableAuthorizationServer
-// serves the user resource, used during client authentication
-@EnableResourceServer
-public class AuthenticationServerApplication{
-	
+public class AuthenticationServerApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationServerApplication.class, args);
 	}
-	
+
+	/**
+	 * Custom FilterRegistrationBean bean permits all OPTIONS requests.
+	 * 
+	 * TODO: On review AL queried this as potentially being a bit heavy handed. BR to follow up with investigation.
+	 */
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+
+	/**
+	 * Configuration class to provide form login
+	 */
 	@Configuration
-	public static class CustomSecurityConfiguration extends AuthorizationServerConfigurerAdapter{
-	
+	public static class LoginSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			//@formatter:off
+			http
+				.formLogin().permitAll()
+				.and().requestMatchers().antMatchers("/login", "/oauth/authorize")
+				.and().authorizeRequests().anyRequest().authenticated();
+			//@formatter:on
+
+		}
+
+	}
+
+	/**
+	 * Configuration class to protect resources.
+	 * 
+	 * NB: extending ResourceServerConfigurer as suggested by
+	 * '@EnableResourceServer interferes with the formLogin provider configured
+	 * by
+	 * {@link rohan.oauth2.authenticationserver.AuthenticationServerApplication.LoginSecurityConfiguration }
+	 */
+	@Configuration
+	@EnableResourceServer
+	public static class ResourceSecurityConfiguration {
+
+	}
+
+	@Configuration
+	@EnableAuthorizationServer
+	public static class CustomSecurityConfiguration extends AuthorizationServerConfigurerAdapter {
+
 		@Autowired
 		@Qualifier("customUserDetailsService")
 		private UserDetailsService userDetailsService;
-		
+
 		@Autowired
 		private AuthenticationManager authenticationManager;
-		
+
 		@Autowired
 		TokenStore tokenStore;
-		
+
 		@Override
 		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-			
+
+			// TODO: remove if password grants flow removed
 			// provides the password grants - I'm not sure why, but it's needed.
 			endpoints.authenticationManager(authenticationManager);
-			
-			// hooks our customer user details service into the basic authentication filter
+
+			// hooks our custom user details service into the basic
+			// authentication filter
 			endpoints.userDetailsService(userDetailsService);
-			
-			// allows logout to reference the token store	
+
+			// allows logout to reference the token store
 			endpoints.tokenStore(tokenStore);
 		}
-		
-		// registers the application (... aka the "client") as an approved requester
+
+		/*
+		 * registers the application (... aka the "client") as an approved
+		 * requester
+		 */
+		//
 		@Override
 		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 			// @formatter:off
-			clients.inMemory()
-				.withClient("acme")
-					.scopes("open")
-					.authorizedGrantTypes("password", "refresh_token")
-					.autoApprove(true)
-					.accessTokenValiditySeconds(500)
+			clients.inMemory().withClient("acme").secret("secret").scopes("open")
+					.authorizedGrantTypes("authorization_code").autoApprove(true).accessTokenValiditySeconds(5)
 					.refreshTokenValiditySeconds(500);
 			// @formatter:on
 		}
-		
-		@Bean
-		public FilterRegistrationBean corsFilter() {
-			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-			CorsConfiguration config = new CorsConfiguration();
-			config.setAllowCredentials(true);
-			config.addAllowedOrigin("*");
-			config.addAllowedHeader("*");
-			config.addAllowedMethod("*");
-			source.registerCorsConfiguration("/**", config);
-			FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-			bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-			return bean;
-		}
-		
+
 		@Bean
 		public TokenStore tokenStore() {
-		    return new InMemoryTokenStore();
+			return new InMemoryTokenStore();
 		}
 	}
 }
--- mvc-ui/src/main/java/rohan/oauth2/mvcui/MvcUiApplication.java
+++ mvc-ui/src/main/java/rohan/oauth2/mvcui/MvcUiApplication.java
@@ -2,68 +2,45 @@ package rohan.oauth2.mvcui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class MvcUiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MvcUiApplication.class, args);
 	}
 
-	@Configuration
-	@CustomEnableOAuth2Sso
-	public static class MvcUiSecurityConfiguration extends WebSecurityConfigurerAdapter {
-		
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			// @formatter:off
-			http.authorizeRequests()
-					.antMatchers("/home", "/").hasRole("USER")
-					.anyRequest()
-					.authenticated()
-				.and()
-					.formLogin()
-						.loginPage("/login")
-						.permitAll()
-						.successForwardUrl("/home")
-				.and()
-					.csrf()
-						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-			// @formatter:on
-		}
-		
-		@Bean
-		public FilterRegistrationBean corsFilter() {
-			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-			CorsConfiguration config = new CorsConfiguration();
-			config.setAllowCredentials(true);
-			config.addAllowedOrigin("*");
-			config.addAllowedHeader("*");
-			config.addAllowedMethod("*");
-			source.registerCorsConfiguration("/**", config);
-			FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-			bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-			return bean;
-		}
-		
-		@Bean
-		protected OAuth2RestTemplate OAuth2RestTemplate(
-		    OAuth2ProtectedResourceDetails resource, OAuth2ClientContext context) {
-		  return new OAuth2RestTemplate(resource, context);
-		}
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+
+	@Bean
+	protected OAuth2RestTemplate OAuth2RestTemplate(OAuth2ProtectedResourceDetails resource,
+			OAuth2ClientContext context) {
+		return new OAuth2RestTemplate(resource, context);
 	}
 
 }
